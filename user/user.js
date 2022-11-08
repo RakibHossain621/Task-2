8fc56dc383a6a282e1746af71eb4fb25a4de7e69
RakibHossain621 <rakibhasanrasel07@gmail.com>
2022-11-08 17:03:56 +0600

    Task 2 commit

---
+++ user/user.js
@@ -0,0 +1,10 @@
+let postDisplay = document.querySelector("#display");
+
+async function getResult(){
+    let acc = await fetch('https://jsonplaceholder.typicode.com/albums');
+    let jsonData= await acc.json();
+    let countData =jsonData.slice(0,10);
+    let Data = JSON.stringify(countData);
+
+     postDisplay.innerText = Data;
+}
